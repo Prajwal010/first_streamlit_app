2daeaf0a47119f5dd1214160669179852cc623ef
Prajwal010 <91496751+Prajwal010@users.noreply.github.com>
2023-04-19 11:00:18 +0530

    Update streamlit_app.py
    Added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Eggs')
-streamlit.text('Bread')
-streamlit.text('Milk')
+streamlit.header(🥣'Breakfast Menu')
+streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🐔'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🥑🍞'Hard-Boiled Free-Range Egg')
