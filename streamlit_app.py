8220f80d17cd122052eaddc0e7c236a6a73d1c60
Prajwal_Gedam <91496751+Prajwal010@users.noreply.github.com>
2023-09-12 17:56:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kivi")
 
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
+# output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
